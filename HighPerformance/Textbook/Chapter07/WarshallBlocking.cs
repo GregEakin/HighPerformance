dc3cbed37db8e92ad301553e1b4e5ce94d4d8396
Greg Eakin <greg@gdbtech.info>
2017-01-02 08:38:34 -0800

    Fix the names of the loop vars.

--- HighPerformance/Textbook/Chapter07/WarshallBlocking.cs
+++ HighPerformance/Textbook/Chapter07/WarshallBlocking.cs
@@ -17,11 +17,11 @@ namespace HighPerformance.Textbook.Chapter07
             if (size != data.GetLength(1))
                 throw new ArgumentException(nameof(data));
 
-            for (var i = 0; i < size; i++)
-                for (var j = 0; j < size; j++)
-                    if (data[j, i])
-                        for (var k = 0; k < size; k++)
-                            data[j, k] = data[j, k] || data[i, k];
+            for (var k = 0; k < size; k++)
+                for (var i = 0; i < size; i++)
+                    if (data[i, k])
+                        for (var j = 0; j < size; j++)
+                            data[i, j] = data[i, j] || data[k, j];
         }
 
     }
