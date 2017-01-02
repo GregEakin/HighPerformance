417b5ec9e9add5e294326d25f50c5d30c254945e
Greg Eakin <greg@gdbtech.info>
2017-01-02 07:13:26 -0800

    Add the WarshallBlocking sample.

---
+++ HighPerformance/Textbook/Chapter07/WarshallBlocking.cs
@@ -0,0 +1,28 @@
+﻿// Greg Eakin
+// January 2, 2017
+
+using System;
+
+namespace HighPerformance.Textbook.Chapter07
+{
+    public class WarshallBlocking
+    {
+        /// <summary>
+        /// Inspaired by Example 7-17 Warshall's algorithm
+        /// </summary>
+        /// <param name="data">The 2-d array of paths through the graph to be reduced.</param>
+        public static void WarshallAlgorithm(bool[,] data)
+        {
+            var size = data.GetLength(0);
+            if (size != data.GetLength(1))
+                throw new ArgumentException(nameof(data));
+
+            for (var i = 0; i < size; i++)
+                for (var j = 0; j < size; j++)
+                    if (data[j, i])
+                        for (var k = 0; k < size; k++)
+                            data[j, k] = data[j, k] || data[i, k];
+        }
+
+    }
+}
---
+++ HighPerformanceTests/Textbook/Chapter07Tests/WarshallBlockingTest.cs
@@ -0,0 +1,24 @@
+﻿// Greg Eakin
+// January 2, 2017
+
+using HighPerformance.Textbook.Chapter07;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HighPerformanceTests.Textbook.Chapter07Tests
+{
+    [TestClass]
+    public class WarshallBlockingTest
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var data = new bool[3, 3];
+            data[0, 1] = true;
+            data[1, 2] = true;
+            
+            WarshallBlocking.WarshallAlgorithm(data);
+
+            Assert.IsTrue(data[0,2]);
+        }
+    }
+}
